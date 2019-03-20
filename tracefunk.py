4d99467881b135712c2d60e9300c4765acf5fe1d
DariaGorbach <43273648+DariaGorbach@users.noreply.github.com>
2019-03-20 23:22:32 +0300

    Update tracefunk.py

--- tracefunk.py
+++ tracefunk.py
@@ -3,14 +3,14 @@ import sys
 
        
 def tracefunk(frame, event, *args):
-    func_name = frame.f_code.co_name
+    func.__name__ = frame.f_code.co_name
     if event == 'return':
         print("function: {}, local_vars: {}".format(func_name, ", ".join("{}".format(loc) for loc in frame.f_locals.keys)))
     return tracefunk
 
 
-def stop(tracefunk):   
+def stop(func):   
     sys.settrace(tracefunk)
-    tracefunk()
+    func()
     sys.settrace(None)
     
